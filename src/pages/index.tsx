554df0f9a539691132c1dd164f198bcabc847cd4
JuanParraIV <jmparra1993@gmail.com>
2023-04-06 10:24:40 -0500

    feat: Add Auth with Clerk

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,8 +16,11 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+
         {!user.isSignedIn ? <SignInButton /> : null}
+
         {!!user.isSignedIn ? <SignOutButton /> : null}
+        
         <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
       </main>
     </>
