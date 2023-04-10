79f2dd7b71f743d0b5b2cc5a192781eea741309b
JuanParraIV <jmparra1993@gmail.com>
2023-04-10 10:46:24 -0500

    feat: Zod, useMutation, Sorting the feed and OnSucess

--- src/components/createPostWizard.tsx
+++ src/components/createPostWizard.tsx
@@ -1,10 +1,20 @@
+import { api } from '@/utils/api';
 import { useUser } from '@clerk/nextjs';
 import Image from 'next/image';
-import React from 'react'
+import React, { useContext, useState } from 'react'
 
 const CreatePostWizard = () => {
 
+  const [input, setInput] = useState("")
   const { user } = useUser();
+
+  const ctx= api.useContext()
+  const { mutate, isLoading: isPosting } = api.post.create.useMutation({
+    onSuccess: ()=>{
+      setInput(''),
+      ctx.post.getAll.invalidate()
+    }
+  })
 
   if (!user) return null;
 
@@ -17,7 +27,18 @@ const CreatePostWizard = () => {
         width={56}
         height={56}
       />
-      <input className='bg-transparent grow outline-none' placeholder="Type some emojis!" />
+      <input
+        className='bg-transparent grow outline-none'
+        placeholder="Type some emojis!"
+        type='text'
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        disabled={isPosting}
+      />
+      <button
+      className='p-5 bg-purple-600 rounded-full'
+      onClick={()=>mutate({content: input})}
+      > Post</button>
     </div>
   )
 }
